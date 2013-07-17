86010f1a1fc4e5f6dca294cb5f223d127bff19ac
rektide <rektide@voodoowarez.com>
2013-07-16 22:07:58 -0400

    extend thennable.then to submit itself up-context.

--- streamnexus.js
+++ streamnexus.js
@@ -5,3 +5,29 @@ function inboundQueue(q){
 		this.inbound.put(el)
 	}
 }
+
+
+//onePromise.then(function(state){})
+
+var Q= require("q")
+
+(function(){
+	var haveQThen= Q.then
+	Q.then= function(val){
+		this.inbound.put(val)
+		return haveQThen.call(this,val)
+	}
+}())
+var then
+
+/*
+q._enhance(_"inboundQueue",function(q){
+	q._chains.get.chains.unshift(function(ctx){
+		if(ctx.val=="then"){
+			then= this
+		}
+	})
+	q._ch
+}
+
+*/
